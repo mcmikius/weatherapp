d701752f2b766bace160a7224c3236f0e3ebf8be
Michail Bondarenko <mcmikius15@gmail.com>
2019-02-12 09:41:59 +0200

    add layout and IB Action

---
+++ WeatherApp/WeatherApp/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by Michail Bondarenko on 2/12/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+    let temperature: Double
+    let appearentTemperature: Double
+    let humidity: Double
+    let pressure: Double
+    let weatherIcon: UIImage
+}
---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Michail Bondarenko on 2/12/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
