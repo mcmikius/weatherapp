3c67eb341460427e08ff53341328cd57c4d319a7
Michail Bondarenko <mcmikius15@gmail.com>
2019-02-12 11:47:28 +0200

    Extension for protocol Weather is done

--- WeatherApp/WeatherApp/APIManager.swift
+++ WeatherApp/WeatherApp/APIManager.swift
@@ -37,6 +37,41 @@ extension APIManager {
                 completionHandler(nil, nil, error)
                 return
             }
+            if data == nil {
+                if let error = error {
+                    completionHandler(nil, HTTPResponse, error)
+                }
+            } else {
+                switch HTTPResponse.statusCode {
+                case 200:
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: AnyObject]
+                        completionHandler(json, HTTPResponse, nil)
+                    } catch let error as NSError {
+                        completionHandler(nil, HTTPResponse, error)
+                    }
+                default:
+                    print("We have got response status \(HTTPResponse.statusCode)")
+                }
+            }
         }
+        return dataTask
+    }
+    func fetch<T>(request: URLRequest, parse: ([String: AnyObject]) -> T?, completionHandler: (APIResult<T>) -> Void) {
+        let dataTask = JSONTaskWith(request: request) { (json, response, error) in
+            guard let json = json else {
+                if let error = error {
+                    completionHandler(.Failure(error))
+                }
+                return
+            }
+            if let value = parse(json) {
+                completionHandler(.Success(value))
+            } else {
+                let error = NSError(domain: MCNetworkingErrorDomain, code: 200, userInfo: nil)
+                completionHandler(.Failure(error))
+            }
+        }
+        dataTask.resume()
     }
 }
