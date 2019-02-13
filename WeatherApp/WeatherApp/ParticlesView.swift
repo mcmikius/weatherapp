66ba1491a880f9db89b947d3a96741d8e5933f8f
Michail Bondarenko <mcmikius15@gmail.com>
2019-02-13 09:28:58 +0200

    Add SpriteKit with rain effect

---
+++ WeatherApp/WeatherApp/ParticlesView.swift
@@ -0,0 +1,14 @@
+//
+//  ParticlesView.swift
+//  WeatherApp
+//
+//  Created by Michail Bondarenko on 2/13/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ParticlesView: SKView {
+
+}
